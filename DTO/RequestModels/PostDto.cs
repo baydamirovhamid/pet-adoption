6f3119311af1e60702e15dabf62443a6281f23fe
Hamid Baydamirov <baydamirovv35@gmail.com>
2024-06-07 01:08:01 +0400

    deleted unnecessary files

--- DTO/RequestModels/PostDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace animal.adoption.api.DTO.RequestModels
-{
-    public class PostDto
-    {
-        public int Id { get; set; }
-        public string? Author { get; set; }
-        public string? Content { get; set; }
-    }
-}
